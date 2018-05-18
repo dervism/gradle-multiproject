db780a3bc6d166328d451d2f508308193c5f14ed
Dervis M <dervis4@gmail.com>
2018-05-19 01:26:44 +0200

    Initial

---
+++ java-app/src/main/java/no/dervis/java/app/Main.java
@@ -0,0 +1,9 @@
+package no.dervis.java.app;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World, from Java-App.");
+    }
+
+}
---
+++ kotlin-app/src/main/kotlin/no/dervis/kotlin/app/Main.kt
@@ -0,0 +1,6 @@
+@file:JvmName("Main")
+package no.dervis.kotlin.app
+
+fun main(vararg args: String) {
+    println("Hello World, from Kotlin-App.")
+}
